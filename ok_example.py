526de738dd87c1388b08bbbfd126742412a5d435
Edward Jaros <edjaros@gmail.com>
2017-09-19 08:15:03 -0500

    improve examples

--- ok_example.py
+++ ok_example.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python2.7
-""" simple demonstration of compass test class """
+""" simple demonstration of compass test class all passing"""
 
 from datatest import DataTest
 import pandas as pd
 
 TDF = pd.DataFrame({'char':['a', 'b', 'c'], 'num':[1, 2, 3]}, index=['j1', 'j2', 'j3'])
 
+# print TDF
+# produces
+#    char  num
+# j1    a    1
+# j2    b    2
+# j3    c    3
+
 DATA_TEST = DataTest(TDF)
 DATA_TEST.equals('j1', 'char', 'a')
-DATA_TEST.equals('j1', 'num', 1) # this test passes
+DATA_TEST.equals('j1', 'num', 1)
 DATA_TEST.matches('j1', 'char', r'\D')
 DATA_TEST.matches('j1', 'num', r'\d')
 DATA_TEST.matches_all('char', '[a-z]')
+DATA_TEST.matches_all('num', '[0-9]')
 DATA_TEST.finish()
