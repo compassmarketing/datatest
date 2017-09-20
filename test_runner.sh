41a911642b415d81e9d4d7b0fcb881e3fb6f3d2c
Edward Jaros <edjaros@gmail.com>
2017-09-20 13:50:05 -0500

    allow specifying tests to test_runner

--- test_runner.sh
+++ test_runner.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-for filename in *_datatest.py; do
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	echo "$filename"
-	echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-	/usr/bin/env python2 "$filename"
-done
+if [ $# -eq 0 ]
+then
+	for filename in *_datatest.py; do
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		echo "$filename"
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		/usr/bin/env python2 "$filename"
+	done
+else
+	for arg in "$@"
+	do
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		echo "$arg"
+		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+		/usr/bin/env python2 "$arg"
+	done
+fi
