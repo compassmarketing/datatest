d636b38409dbb026dab4de0ea587ccb6e9e6f25b
Edward Jaros <edjaros@gmail.com>
2017-09-20 14:45:52 -0500

    allow specifying tests; populate test_runner fail code

--- test_runner.sh
+++ test_runner.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+
+failcount=0
 if [ $# -eq 0 ]
 then
 	for filename in *_datatest.py; do
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		echo "$filename"
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-		/usr/bin/env python2 "$filename"
+		
+		if ! /usr/bin/env python2 "$filename"
+		then
+			let failcount++
+		fi
 	done
 else
 	for arg in "$@"
@@ -13,6 +19,12 @@ else
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 		echo "$arg"
 		echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-		/usr/bin/env python2 "$arg"
+
+		if ! /usr/bin/env python2 "$arg"
+		then
+			let failcount++
+		fi
 	done
 fi
+
+exit $failcount
